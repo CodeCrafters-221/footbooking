f53260ae58268d74843ce2ed2f31a063796132de
Joyboy42-8 <cheikhoumamouciss@gmail.com>
2026-04-23 19:14:16 +0000

    Adding socials links: tiktok and instagram

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -71,7 +71,7 @@ export default function Footer() {
 
               <li className="flex items-center gap-2">
                 <Mail className="text-primary w-4 h-4" />
-                  <a href="mailto:code.crafters221@gmail.com?subject=Bienvenue, comment pouvons-nous vous aider ?&body=FootBooking vous accompagne pour toutes vos réservations de terrains.">contact@footbooking.sn</a>
+                <a href="mailto:footbooking.sn@gmail.com?subject=Bienvenue, comment pouvons-nous vous aider ?&body=FootBooking vous accompagne pour toutes vos réservations de terrains.">contact@footbooking.sn</a>
               </li>
 
               <li className="flex items-center gap-2">
@@ -79,36 +79,21 @@ export default function Footer() {
                 <a href="tel:+221760263631">+221 76 026 36 31</a>
               </li>
             </ul>
-
+            {/* https://www.instagram.com/footbooking */}
+            {/* https://www.tiktok.com/@footbooking221?_r=1&_t=ZN-95mlngwTKZK */}
             {/* SOCIAL */}
-            {/* <div className="flex gap-3 mt-2">
-              <a
-                className="size-10 rounded-full bg-[#2e2318] flex items-center justify-center text-text-secondary hover:bg-primary hover:text-[#231a10] transition-colors"
-                href="#"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="size-5"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"></path>
-                </svg>
+            <div className="flex gap-3 mt-2">
+              <a className="size-10 rounded-full bg-[#2e2318] flex items-center justify-center text-text-secondary hover:bg-primary hover:text-[#231a10] transition-colors"
+                href="https://www.instagram.com/footbooking">
+                <Instagram />
               </a>
-              <a
-                className="size-10 rounded-full bg-[#2e2318] flex items-center justify-center text-text-secondary hover:bg-primary hover:text-[#231a10] transition-colors"
-                href="#"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="size-5"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772 4.902 4.902 0 011.772-1.153c.636-.247 1.363-.416 2.427-.465 1.067-.047 1.409-.06 3.809-.06s2.783.013 3.809.06zm-1.983 4.927a3.076 3.076 0 100 6.152 3.076 3.076 0 000-6.152zM17.153 8.16a1.165 1.165 0 11-2.33 0 1.165 1.165 0 012.33 0zm-8.82 8.357a5.365 5.365 0 1110.73 0 5.365 5.365 0 01-10.73 0z"></path>
-                </svg>
+              <a className="size-10 rounded-full bg-[#2e2318] flex items-center justify-center text-text-secondary hover:bg-primary hover:text-[#231a10] transition-colors"
+                href="https://www.tiktok.com/@footbooking221?_r=1&_t=ZN-95mlngwTKZK">
+                <i class="fa-brands fa-tiktok fa-lg" 
+                  style={{color: "rgb(203, 173, 144)"}}>
+                </i>
               </a>
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
